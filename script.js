57642063b43ba82463d444fc89b31a0529de1bb5
Eugene Chernik <evgcher1994@gmail.com>
2023-11-14 16:27:00 +0300

    Fixed js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-
 // Модальное окно
 const modal = document.getElementById("modal"); // Кнопка открытия
 const btns = document.getElementsByClassName("btn-reserve"); // Кнопки модального окна
 const span = document.getElementsByClassName("close"); // Кнопки закрытия
-// Изменение стилей (отображения) при клике
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+
 // Если кликнуть на модальном окне, оно отключится
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -29,20 +22,12 @@ function closeNav() {
 // Перебор кнопок
 for(let elem of btns){
   elem.onclick = function() {
-    if (modal.style.display === "block") {
-      modal.style.display = "none";
-    } else {
-      modal.style.display = "block";
-    }
-  };
+    modal.style.display = "block"
+  }
 }
 // Кнопки закрытия
 for(let close of span){
   close.onclick = function() {
-    if (modal.style.display === "block") {
-      modal.style.display = "none";
-    } else {
-      modal.style.display = "block";
-    }
-  };
+    modal.style.display = "none"
+  }
 }
